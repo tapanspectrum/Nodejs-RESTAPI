9dd2c22998fffa257fea9fc5a7a6bc39a08f3a14
tapanspectrum@gmail.com <tapanspectrum@gmail.com>
2021-06-16 09:30:57 +0530

    pino levels added

--- server/common/logger.js
+++ server/common/logger.js
@@ -3,6 +3,13 @@ import pino from 'pino';
 const l = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
-});
+},pino.destination('./logFile'));
+l.fatal('fatal')
+l.error('error')
+l.warn('warn')
+l.info('info')
+l.debug('debug')
+l.trace('trace')
+l.useLevelLabels
 
 export default l;
